4c837dab4da017ba86f51ced0f5dcad023b9a854
yaseen ahmed <yaseennasri8@gmail.com>
2023-04-06 11:13:32 +0500

    method design for cli tool

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,7 @@ const kualiteeJson = require(`${process.cwd()}/package.json`);
 
 const endPoint = 'https://apiss.kualiteestaging.com/api/v2/test_case/automation_testcase_report_execution'
 
-function readCucumberJsonDirectory(): Promise<any[]> {
-
-  const user_token = kualiteeJson.kualiteeConfigs.token;
-  const project_id = kualiteeJson.kualiteeConfigs.projectId;
-  const directoryPath = kualiteeJson.kualiteeConfigs.reportPath;
+function readReportDirectory(user_token: string,project_id: string,directoryPath: string): Promise<any[]> {
 
   const promises: Promise<any>[] = [];
   const files = fs.readdirSync(directoryPath);
@@ -38,14 +34,34 @@ function readCucumberJsonDirectory(): Promise<any[]> {
   return Promise.all(promises);
 }
 
-export const postResult = async () => {
+export const postReport = async () => {
   try {
-    return await readCucumberJsonDirectory().then((response) => {
+    const user_token = kualiteeJson.kualiteeConfigs.token;
+    const project_id = kualiteeJson.kualiteeConfigs.projectId;
+    const directoryPath = kualiteeJson.kualiteeConfigs.reportPath;
+    return await readReportDirectory(user_token,project_id,directoryPath).then((response) => {
       if(response.length && response.length > 0) {
         const responseObject = response[0].data;
         console.log('\x1b[32m%s\x1b[0m',`${responseObject.message} on Kualitee Tool`);
         return;
-    }
+      }
+      console.log('\x1b[32m%s\x1b[0m',"Report Generated Successfully on Kualitee Tool")
+    }).catch ((error) => {
+      console.log('\x1b[41m\x1b[37m%s\x1b[0m',`${error.response.data.errors}, Report cannot be generated on Kualitee Tool` || "failed with errors to generate report on Kualitee Tool ")
+  });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const postResult = async (user_token: string,project_id: string,directoryPath: string) => {
+  try {
+    return await readReportDirectory(user_token,project_id,directoryPath).then((response) => {
+      if(response.length && response.length > 0) {
+        const responseObject = response[0].data;
+        console.log('\x1b[32m%s\x1b[0m',`${responseObject.message} on Kualitee Tool`);
+        return;
+      }
       console.log('\x1b[32m%s\x1b[0m',"Report generated Successfully on Kualitee Tool")
     }).catch ((error) => {
       console.log('\x1b[41m\x1b[37m%s\x1b[0m',`${error.response.data.errors}, Report cannot be generated on Kualitee Tool` || "failed with errors to generate report on Kualitee Tool ")
