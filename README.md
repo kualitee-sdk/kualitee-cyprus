1fdb99d0de55d53ff869af618554c2a3034f8d55
yaseen ahmed <yaseennasri8@gmail.com>
2023-04-26 17:58:22 +0500

    execution method implemented and restructured the doc file

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # kualitee_cypress
-This package is for Kualitee users to post their execution cycle from cypress to Kualitee tool.
+This package is for [Kualitee](https://www.kualitee.com/) users.
+**Core features**
+* To generate report on kualitee from cypress.
+* Integration kualitee with cypress, map the scenario and its status with kualitee if you are using cucumber in cypress.
 
-## Getting started
-### Step 1: Install `kualitee_cypress` :
+## Table of Contents
+- [Installation](#install-kualitee_cypress)
+- [Post report to Kualitee](#post-report-to-kualitee)
+- [Map test case status on Kualitee](#map-test-case-status-on-kualitee)
+
+
+## Install `kualitee_cypress` :
 
 #### NPM
 ```
 npm install kualitee_cypress
 ```
 
-### Step 2: Configure the `kualiteeConfigs` to `package.json` :
+## Post Report to Kualitee
+If you are using [Kualitee](https://www.kualitee.com/) testing tool then you can generate the execution cycle on Kualitee by following these steps.
+### Step 1: Configure the `kualiteeConfigs` to `package.json`
 ```
   "kualiteeConfigs": {
     "token": 'userToken',
@@ -23,10 +33,10 @@ npm install kualitee_cypress
 
 **projectId**: _string_ Project id on Kualitee tool with which the tests are associated.
 
-**reportPath**: _string_ The path in cypress project where reports generate like `cypress/e2e/reports`.
+**reportPath**: _string_ The path in the cypress project where reports generate like `cypress/e2e/reports`.
 
 
-### Step 3: Import or require the postResult from kualitee_cypress :
+### Step 2: Import or require the postResult from kualitee_cypress
 `import` or `require` (as your project demand) the `postResult` method in `cypress.config.js`.
 
 ```
@@ -80,4 +90,84 @@ After configuration `run the cypress` in terminal (`npx cypress run --component`
 On completion of cypress run, you will get the `Successfull` or `Error` message in terminal.
 
 ![Screenshot from 2023-04-06 11-28-07](https://user-images.githubusercontent.com/48677205/230292569-ad41b583-8492-477e-a445-3cea514c1293.png)
+
+## Map Test Case Status on Kualitee
+First, you need to Integrate Kualitee with Cypress, This integration involves configuring Kualitee to run Cypress tests and receive results. So, you need to follow these steps.
+
+**Prerequisites**
+- Installed `kualitee_cypress` in your Cypress project.
+- Cypress using [Cucumber](https://cucumber.io/) and writing the test scenarios in BDD format.
+- Able to run a single _scenario_ by using _tag_ in cypress.
+- You need to add a unique tag for both **Feature** and **Scenario**. It is mandatory.  
+
+### Step 1: Add tags
+You need to add a unique tag for both _feature_ and _scenario_ with __@kts__ or __@ktc__. If you want to add tag **@login** for _feature_ then you will have
+to add prefix like `@kts_login` and if you want to add tag **@success_login** for _scenario_ then you have to add the tag like `@ktc_success_login`.This step is mandatory for the _scenario_ or _feature_ which you want to execute and get status on Kualitee.
+### Step 1: Host your cypress project on a server
+This is mandatory to host your cypress project on the server by which you can run your *scenarios* from Kualitee by *tags*.
+
+### Step 2: Set up an express server
+Set up an express server and expose an API. Following is the example of an express server you can use it as template.
+
+```
+const express = require('express')
+var cors = require('cors');
+var bodyParser = require('body-parser')
+const app = express()
+app.use(cors());
+app.use(bodyParser.json())
+
+const port = 3000
+const hostname = 'localhost';
+
+app.post('/script-run', (req, res) => {
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
+```
+**note:-** Here you need to design proper route in **app.post()** function and use it in integration Kualitee with Cypress.
+
+### Step 3: Include `kualitee_cypress`
+If you are working in _CommonJS_ environment then include in server file like
+```
+const {executeTestCase} = require("kualitee_cypress")
+```
+If you are working in _ES6 (ECMAScript 2015) module system_ the include in server file like
+```
+import { executeTestCase } from 'kualitee_cypress';
+```
+
+### Step 4: Call the method in `kualitee_cypress`
+You need to call the method _executeTestCase_ in  express **app.post()** function routes (which you used in kualitee to integrate kualitee with cypress)
+with three arguments.
+
+```
+app.post('/script-run', (req, res) => {
+  executeTestCase(req, res, 'cypress/e2e/reports')
+});
+```
+#### Arguments
+**req**: It is the reqest body and it will pass as it is *req*.
+
+**req**: It is the response body and it will pass as it is *res*.
+
+**reportPath**: _string_ The path in the cypress project where reports generate like `cypress/e2e/reports`.
+Now you can execute the _Scenario_ from Kualitee tool.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import * as fs from 'fs';
 import * as path from 'path';
 import FormData from 'form-data'
 import axios from 'axios';
+import { spawn } from 'child_process';
+
+import ps from 'ps-node';
+import { Request, Response } from 'express';
 
 
-const endPoint = 'https://apiss.kualiteestaging.com/api/v2/test_case/automation_testcase_report_execution'
+
 
 function readReportDirectory(user_token: string, project_id: string, directoryPath: string): Promise<any[]> {
+  const endPoint = 'https://apiss.kualiteestaging.com/api/v2/test_case/automation_testcase_report_execution'
 
   try {
     const promises: Promise<any>[] = [];
@@ -42,6 +47,45 @@ function readReportDirectory(user_token: string, project_id: string, directoryPa
   }
 }
 
+function updateStatusOnKualitee(reportPath: string, body: any) {
+  const endPoint = 'https://apiss.kualiteestaging.com/api/v2/test_case_execution/execute_automatic';
+  const promises: Promise<any>[] = [];
+
+  const files = fs.readdirSync(reportPath);
+  files.forEach(file => {
+    const filePath = path.join(reportPath, file);
+    const splitFile = file.split('.');
+
+    try {
+      if (fs.statSync(filePath).isFile() && splitFile.includes('cucumber') && path.extname(filePath) === '.json') {
+        const fileForm = new FormData();
+        fileForm.append('token', body.token);
+        fileForm.append('project_id', body.project_id)
+        fileForm.append('execute', body.execute)
+        fileForm.append('cycle_id', body.cycle_id)
+        body.tc_ids.forEach((id: any) => {
+          fileForm.append('tc_ids[]', id);
+        });
+        body.tc_tags.forEach((tag: any) => {
+          fileForm.append('tc_tags[]', tag);
+        });
+        fileForm.append('report', fs.createReadStream(filePath));
+        const promise = axios.post(endPoint, fileForm, {
+          headers: {
+            'content-type': 'multipart/form-data'
+          }
+        }).then(response => response.data);
+
+        promises.push(promise);
+      }
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  return promises;
+}
+
 export const postReport = async () => {
   try {
     const fileContent = fs.readFileSync(`${process.cwd()}/package.json`, 'utf8');
@@ -52,12 +96,30 @@ export const postReport = async () => {
     return await readReportDirectory(user_token, project_id, directoryPath).then((response) => {
       if (response.length && response.length > 0) {
         const responseObject = response[0].data;
-        console.log('\x1b[32m%s\x1b[0m', `${responseObject.message} on Kualitee Tool`);
+        console.log(
+          '\x1b[32m%s\x1b[0m',
+          `\n
+  <=====================================================================================>
+    ${responseObject.message} on Kualitee Tool
+  <=====================================================================================>\n`
+        );
         return;
       }
-      console.log('\x1b[32m%s\x1b[0m', "Report Generated Successfully on Kualitee Tool")
+      console.log(
+        '\x1b[32m%s\x1b[0m',
+        `\n
+  <=====================================================================================>
+    Report generated Successfully on Kualitee Tool
+  <=====================================================================================>\n`
+      );
     }).catch((error) => {
-      console.log('\x1b[41m\x1b[37m%s\x1b[0m', `${error.response.data.errors}, Report cannot be generated on Kualitee Tool` || "failed with errors to generate report on Kualitee Tool ")
+      console.log(
+        '\x1b[41m\x1b[37m%s\x1b[0m',
+        `\n
+<=====================================================================================>
+${error.response.data.errors}, Report cannot be generated on Kualitee Tool
+<=====================================================================================>\n`
+      );
     });
   } catch (error) {
     console.error(error);
@@ -69,14 +131,88 @@ export const postResult = async (user_token: string, project_id: string, directo
     return await readReportDirectory(user_token, project_id, directoryPath).then((response) => {
       if (response.length && response.length > 0) {
         const responseObject = response[0].data;
-        console.log('\x1b[32m%s\x1b[0m', `${responseObject.message} on Kualitee Tool`);
+        console.log(
+          '\x1b[32m%s\x1b[0m',
+          `\n
+  <=====================================================================================>
+    ${responseObject.message} on Kualitee Tool
+  <=====================================================================================>\n`
+        );
         return;
       }
-      console.log('\x1b[32m%s\x1b[0m', "Report generated Successfully on Kualitee Tool")
+      console.log(
+        '\x1b[32m%s\x1b[0m',
+        `\n
+  <=====================================================================================>
+    Report generated Successfully on Kualitee Tool
+  <=====================================================================================>\n`
+      );
     }).catch((error) => {
-      console.log('\x1b[41m\x1b[37m%s\x1b[0m', `${error.response.data.errors}, Report cannot be generated on Kualitee Tool` || "failed with errors to generate report on Kualitee Tool ")
+      console.log(
+        '\x1b[32m%s\x1b[0m',
+        `\n
+        <=====================================================================================>
+        ${error.response.data.errors}, Report cannot be generated on Kualitee Tool
+        <=====================================================================================>\n`
+      );
     });
   } catch (error) {
     console.error(error);
+  }
+}
+
+export const executeTestCase = (req: Request, res: Response, reportPath: string) => {
+  try {
+    const response = res;
+    if (req.body.ping) {
+      res.send({ status: true, message: 'Server working properly...' })
+    } else {
+      ps.lookup({ command: 'cypress' }, (err, resultList) => {
+        if (err) {
+          throw err;
+        }
+
+        // If there are any running processes, send an error response
+        if (resultList.length > 0) {
+          response.status(503).send({ status: false, message: 'The execution of testing is already in process.Please try in while..' })
+        } else {
+          const { body } = req;
+          let tag = '';
+          if (body.tc_tags.length > 1) {
+            tag = body.tc_tags.join(" or ");
+          } else {
+            tag = body.tc_tags[0];
+          }
+
+          const cypressArgs = ['cypress', 'run', '--env', `TAGS="${tag}"`];
+          const cypress = spawn('npx', cypressArgs);
+
+          // log Cypress output to console
+          cypress.stdout.on('data', (data) => {
+            console.log(data.toString());
+          });
+
+          // log Cypress errors to console
+          cypress.stderr.on('data', (error) => {
+            response.send(error)
+            process.exit(1);
+          });
+
+          cypress.on('close', async (code) => {
+            const onCloseResponse = res;
+            const promises = updateStatusOnKualitee(reportPath, body);
+
+            try {
+              await Promise.all(promises);
+              onCloseResponse.status(200).send({ status: true, message: `Test case with TAG ${body.tc_tags} executed successfully. ` });
+            } catch (error: any) {
+              onCloseResponse.status(400).send({ status: false, message: error.message });
+            }
+          });
+        }
+      });
+    }
+  } catch (error) {
+    throw error;
   }
 }
